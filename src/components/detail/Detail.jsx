5bbe4d4dd64cd1612cc81778ca15ffee2df801f3
Enigma-de <aayusharegmi2@gmail.com>
2024-10-03 18:27:13 +0545

    Detailed section added : includes UI of chat details and user section.

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -5,7 +5,7 @@ return (
         <div className="user">
             <img src="./avatar.png" alt="" srcset="" />
             <h2>Aayusha Regmi</h2>
-            <p>Lorem ipsum dolor sit amet consectetur </p>
+            <p>Lorem ipsum dolor sit  </p>
         </div>
         <div className="info">
             <div className="option">
@@ -18,29 +18,59 @@ return (
             <div className="option">
                 <div className="title">
                     <span>Privacy % help</span>
-                    <img src="./arrowUp.png" alt="" srcset="" />
+                    <img src="./arrowUp.png" alt="" />
                 </div>
             </div>
 
             <div className="option">
                 <div className="title">
                     <span>Shared Photos</span>
-                    <img src="./arrowDown.png" alt="" srcset="" />
+                    <img src="./arrowDown.png" alt=""  />
                 </div>
             </div>
 <div className="photos">
     <div className="photoItem">
-        <img src="./" alt="" />
-        <span>photo_2024.jpg</span>
+        <div className="photoDetail">
+        <img src="image1.png" alt="" />
+        <span>photo_202.jpg</span>
     </div>
     <img src="./download.png" alt="" />
 </div>
+
+<div className="photoItem">
+        <div className="photoDetail">
+        <img src="image1.png" alt="" />
+        <span>photo_202.jpg</span>
+    </div>
+    <img src="./download.png" alt="" />
+</div>
+
+<div className="photoItem">
+        <div className="photoDetail">
+        <img src="image1.png" alt="" />
+        <span>photo_202.jpg</span>
+    </div>
+    <img src="./download.png" alt="" />
+</div>
+
+<div className="photoItem">
+        <div className="photoDetail">
+        <img src="image1.png" alt="" />
+        <span>photo_202.jpg</span>
+    </div>
+    <img src="./download.png" alt="" />
+</div>
+
+
+
+</div>    
             <div className="option">
                 <div className="title">
-                    <span>Chat Settings</span>
+                    <span>Shared Files</span>
                     <img src="./arrowUp.png" alt="" srcset="" />
                 </div>
             </div>
+            <button>Block User</button>
         </div>
     </div>
 )
