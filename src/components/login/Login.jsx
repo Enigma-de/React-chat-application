f373768ebf7600fad542a14540b04609b200b4ff
Enigma-de <aayusharegmi2@gmail.com>
2024-10-21 23:48:58 +0545

    Login Page UI is half done : also the idea of signing up is mentioned :

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,17 @@
 import "./login.css";
 const Login= ()=> {
 return(
-<div className="login"> LogIn
+<div className="login">
+  <h2> Welcome Back!</h2>  
+  <form>
+    <input type="text" name="username" id="username" placeholder="Username" />
+    <input type="password" name="password" id="password" placeholder="Password" />
+    <input type="button" value="LogIn" />
+  </form>
+  <p className="noaccountline">Don't Have an account? <button className="R\egister">Register</button></p>
+  </div>
 
-</div>
 )
 }
 export default Login;
+// idea: lets do it this way; when user clicks Register lead him to the component signup 
